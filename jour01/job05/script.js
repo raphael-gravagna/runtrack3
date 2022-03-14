c3b1a41d10b990edf3c9ae1c2011724d28671e73
raphael gravagna <raphael.gravagna@laplateforme.io>
2022-03-14 12:37:39 +0100

    job5 ok

---
+++ jour01/job05/script.js
@@ -0,0 +1,12 @@
+function afficherjourssemaines() {
+
+let semaine = ['lundi', 'mardi', 'mercredi', 'jeudi','vendredi', 'samedi', 'dimanche'];
+
+for (let i = 0; i < semaine.length; i++) {
+    console.log(semaine[i]);
+
+}
+
+}
+afficherjourssemaines();
+
