9f3524dd6a3276b978ac57ad54383ec45f4a9891
raphael gravagna <raphael.gravagna@laplateforme.io>
2022-03-14 12:03:35 +0100

    affichage console job4

--- jour01/job04/script.js
+++ jour01/job04/script.js
@@ -3,5 +3,6 @@ function bisextile(annee)
     var annee = "2000"
     if ((annee%4==0) && ((annee%100!=0) || (annee%400==0))) return true;
     else return false;
-
+    
 }
+console.log(bisextile(1996));
