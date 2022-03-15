e3fd29bbc94c5f546d2a1004a9fe7e186a853f08
raphael gravagna <raphael.gravagna@laplateforme.io>
2022-03-15 10:58:32 +0100

    job2ok

---
+++ jour02/job02/script.js
@@ -0,0 +1,10 @@
+function showHide() {
+	const targetDiv = document.getElementById("citation");
+	//var citationHtml = document.getElementById("citation");
+	//console.log(citationHtml.innerHTML);
+	if (targetDiv.style.display !== "none") {
+		targetDiv.style.display = "none";
+	  } else {
+		targetDiv.style.display = "block";
+	  }
+}
