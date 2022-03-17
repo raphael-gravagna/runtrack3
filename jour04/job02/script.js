a5ab8bcf9eb6f7331a7675598726728cb2f5c88d
raphael gravagna <raphael.gravagna@laplateforme.io>
2022-03-17 18:01:44 +0100

    nous allons mieux

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,20 +1,22 @@
-const tableau = {
-name: "La Plateforme_",
-address: "8 rue d'hozier",
-city: "Marseille",
-nb_staff: "11", 
-creation:"2019"
-}
+const tableau = [{"name":"La Plateforme_", "address":"8 rue d'hozier", "city":"Marseille", "nb_staff":"11", "creation":"2019"}]
+//console.log(tableau[0]["city"])
+//document.write(tableau[0]["city"])
+const key = "city"
 
+document.addEventListener("DOMContentLoaded", ()=> { 
+    jsonValueKey(tableau, key);
+})
+
+function jsonValueKey(tableau, key) {
+    document.write(tableau[0][key])
+}
 
 /*console.log(tableau)
 document.write(tableau.name);
 document.write(tableau.city);
 */
+/*
 
-document.addEventListener("DOMContentLoaded", ()=> { 
-    jsonValueKey(tableau);
-})
 
 function jsonValueKey(tableau) {
     document.write(tableau.name);
@@ -22,4 +24,4 @@ function jsonValueKey(tableau) {
     document.write(tableau.city);
     document.write(tableau.nb_staff);
     document.write(tableau.creation);
-}
+}*/
